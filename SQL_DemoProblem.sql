dd218cc79d0b1b9590d4f6adcfbf4ba501dd86ae
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-09 15:59:52 +0530

    [somu]Add:Inserting Data into Table.

--- SQL_DemoProblem.sql
+++ SQL_DemoProblem.sql
@@ -2,3 +2,9 @@
 Create Database RFP_287Batch
 --UC-2
 Create table EngineersData (Id int primary key identity, Name varchar(20),Gender char,Salary Bigint,Address varchar(100))
+select * from EngineersData
+--UC-3
+Insert into EngineersData (Name,Gender,Salary,Address) values('Somashekar','M',25000,'Banglore')
+Insert into EngineersData (Name,Gender,Salary,Address) values('AnilKumar','M',25000,'Banglore')
+Insert into EngineersData (Name,Gender,Salary,Address) values('Anu','F',24000,'Banglore')
+Insert into EngineersData (Name,Gender,Salary,Address) values('Moni','F',24000,'Banglore')
